20ec8cc8460fa24feacc415372ca203a6a2e9195
tokudatsushi08 <atsushi.t08@gmail.com>
2020-03-08 19:07:59 +0900

    firstcommit

---
+++ top.js
@@ -0,0 +1,20 @@
+'user strict'
+{
+    var app = new Vue({
+        el: '#app',
+        data: {
+            message: 'Hello World!',
+            url: 'https://jp.vuejs.org/index.html',
+            toggle: false,
+            languages: [
+                'JavaScript', 'Ruby', 'Phython'
+            ]
+        },
+        methods: {
+            onclick: function () {
+                this.message = "Clicked!"
+            }
+        }
+
+    })
+}
